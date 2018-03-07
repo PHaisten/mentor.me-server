f7f873c1553bdb725e2da0ede3b569301d7fb439
Lizziebot <lizcarmike@gmail.com>
2018-03-07 12:23:31 -0600

    added token and login middleware to routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,14 +9,10 @@ import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 
 let router = Router();
 
-//router.use('/auth', authRouter);
+router.use('/auth', authRouter);
 router.use('/donate', stripeDonationsRouter);
 
-// router
-//   .route("*")
-//   .post(tokenMiddleware, isLoggedIn)
-//   .put(tokenMiddleware, isLoggedIn)
-//   .delete(tokenMiddleware, isLoggedIn);
+router.use(tokenMiddleware, isLoggedIn);
 
 router.use('/topics', topicRouter);
 router.use('/mentees', menteeRouter);
